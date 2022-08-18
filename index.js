cb4a15a16039c88c387d20029af7db85f0ec04ba
SolomonKitonyi <kitonyisolomon@gmail.com>
2022-08-18 14:23:42 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+function submitData(userName,userEmail) {
+    const FormData = {
+        name: userName,
+        email: userEmail
+    }
+    const configurationObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify(FormData)
+    }
+   let p = document.createElement("p")
+   return fetch("http://localhost:3000/users",configurationObj)
+    .then(res => res.json())
+    .then(data => {
+        let p = document.createElement("p")
+        p.textContent = data.id
+        document.querySelector("body").appendChild(p)
+   })
+   .catch(err => {
+    let p = document.createElement("p")
+    p.textContent = err.message;
+    document.querySelector("body").appendChild(p)
+   })
+}
